a57477ed4292fe7ae682238008c68051735f0f32
Prahlad Singh <prahladsingh7512@gmail.com>
2026-08-29 22:41:48 +0530

    Update cwd path for schoolerp-api

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,13 +3,14 @@ module.exports = {
     {
       name: 'schoolerp-api',
       script: 'server.js',
-      cwd: '/home/prahlad/schoolerp/backend',
+      cwd: '/var/www/projects/schoolerp/backend',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '512M',
       env_production: {
         NODE_ENV: 'production',
+        PORT: 5006,
       },
     },
   ],
